b458319b0ae0c6839060b0ffbbdca85387f82209
Larry <larry10hhobh@126.com>
2022-09-10 13:26:04 +0800

    test1

---
+++ hello.go
@@ -0,0 +1,142 @@
+package main
+
+import "fmt"
+
+var a, b = 123, "first测试"
+
+func main() {
+	g, h := 456, "hello测试"
+	fmt.Println(a,b,g,h)
+	fmt.Printf("数字为%d\n",a)
+	//fmt.Println()
+	fmt.Println(a,b,g,h)
+
+	if g<456 {
+		fmt.Println("g<456")
+	} else {
+		fmt.Println("g>=456")
+	}
+
+	var x interface{}
+
+	switch i := x.(type) {
+	case nil:
+		fmt.Printf(" x 的类型 :%T",i)
+	case int:
+		fmt.Printf("x 是 int 型")
+	case float64:
+		fmt.Printf("x 是 float64 型")
+	case func(int) float64:
+		fmt.Printf("x 是 func(int) 型")
+	case bool, string:
+		fmt.Printf("x 是 bool 或 string 型" )
+	default:
+		fmt.Printf("未知型")
+	}
+
+	flag := true
+	//使用 fallthrough 会强制执行后面的 case 语句，fallthrough 不会判断下一条 case 的表达式结果是否为 true。
+	switch {
+	case !flag:
+		fmt.Println("1、case 条件语句为 false")
+		fallthrough
+	case flag:
+		fmt.Println("2、case 条件语句为 true")
+		fallthrough
+	case !flag:
+		fmt.Println("3、case 条件语句为 false")
+		fallthrough
+	case flag:
+		fmt.Println("4、case 条件语句为 true")
+	case !flag:
+		fmt.Println("5、case 条件语句为 false")
+		fallthrough
+	default:
+		fmt.Println("6、默认 case")
+	}
+
+	/*
+	标记
+	 */
+	// 不使用标记
+	fmt.Println("---- break ----")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			break
+		}
+	}
+
+	// 使用标记
+	fmt.Println("---- break label ----")
+	re1:
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			break re1
+		}
+	}
+
+
+	// 不使用标记
+	fmt.Println("---- continue ---- ")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			continue
+		}
+	}
+
+	// 使用标记
+	fmt.Println("---- continue label ----")
+	re2:
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			continue re2
+		}
+	}
+
+
+	for m := 1; m < 10; m++ {
+		for n := 1; n <= m; n++ {
+			fmt.Printf("%dx%d=%d ",n,m,m*n)
+		}
+		fmt.Println("")
+	}
+
+	for m := 1; m < 10; m++ {
+		n := 1
+	LOOP: if n <= m {
+		fmt.Printf("%dx%d=%d ",n,m,m*n)
+		n++
+		goto LOOP
+	} else {
+		fmt.Println("")
+	}
+		//n++
+		//fmt.Println(n)
+	}
+
+	var c1, c2, c3 chan int
+	var i1, i2 int
+	fmt.Println(c1,c2,c3,i1,i2)
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3):  // same as: i3, ok := <-c3
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
+}
